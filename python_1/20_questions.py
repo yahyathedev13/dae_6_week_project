66fcdd97085f6611c5fca6c139a56dbb381abf80
yahyathedev13 <yahya.ahmed.dev.123@gmail.com>
2025-07-16 14:28:23 -0400

    pushing work

--- python_1/20_questions.py
+++ python_1/20_questions.py
@@ -14,48 +14,88 @@ possible_guesses = [
 facts = {
 #                       animals
     "elephant":{"is_animal": True, "is_nature": True, "is_place": False,
-                 "is_food":False, "is_alive":True,"can_fly":False,"has_legs": True},
+                 "is_food":False, "is_alive":True,"can_fly":False,
+                 "has_legs": True
+                 },
     "tiger":{"is_animal": True, "is_nature": True, "is_place": False,
-              "is_food":False, "is_alive":True,"can_fly":False,"has_legs": True},
+              "is_food":False, "is_alive":True,"can_fly":False,
+              "has_legs": True
+              },
     "giraffe":{"is_animal": True, "is_nature": True,"is_place": False,
-                "is_food":False, "is_alive":True,"can_fly":False,"has_legs": True},
+                "is_food":False, "is_alive":True,"can_fly":False,
+                "has_legs": True
+                },
     "monkey":{"is_animal": True, "is_nature": True,"is_place": False,
-               "is_food":False, "is_alive":True,"can_fly":False,"has_legs": True},
-    "hawk":{"is animal": True, "is_nature": True, "is_place": False,
-             "is_food":False, "is_alive":True,"can_fly":True,"has_legs": False},
+               "is_food":False, "is_alive":True,"can_fly":False,
+               "has_legs": True
+               },
+    "hawk":{"is_animal": True, "is_nature": True, "is_place": False,
+             "is_food":False, "is_alive":True,"can_fly":True,
+             "has_legs": False
+             },
 #                         nature
     "tree": {"is_animal": False, "is_nature": True, "is_place": False,
-              "is_food": False, "is_alive": True,"can_fly":False,"has_legs": False},
+              "is_food": False, "is_alive": True,"can_fly":False,
+              "has_legs": False
+              },
     "grass": {"is_animal": False, "is_nature": True, "is_place": False,
-               "is_food": False, "is_alive": True,"can_fly":False,"has_legs": False},
+               "is_food": False, "is_alive": True,"can_fly":False,
+               "has_legs": False
+               },
     "river": {"is_animal": False, "is_nature": True, "is_place": False,
-               "is_food": False, "is_alive": False,"can_fly":False,"has_legs": False},
+               "is_food": False, "is_alive": False,"can_fly":False,
+               "has_legs": False
+               },
     "mountain": {"is_animal": False, "is_nature": True, "is_place": False,
-                  "is_food": False, "is_alive": False,"can_fly":False},
+                  "is_food": False, "is_alive": False,"can_fly":False,
+                  "has_legs": False
+                  },
     "flower": {"is_animal": False, "is_nature": True, "is_place": False,
-                "is_food": False, "is_alive": True,"can_fly":False,"has_legs": False},
+                "is_food": False, "is_alive": True,"can_fly":False,
+                "has_legs": False
+                },
 #                          places
-    "Paris": {"is_animal": False, "is_nature": False, "is_place": True,
-               "is_food": False, "is_alive": False,"can_fly":False,"has_legs": False},
+    "paris": {"is_animal": False, "is_nature": False, "is_place": True,
+               "is_food": False, "is_alive": False,"can_fly":False,
+               "has_legs": False
+               },
     "school": {"is_animal": False, "is_nature": False, "is_place": True,
-                "is_food": False, "is_alive": False,"can_fly":False,"has_legs": False},
+                "is_food": False, "is_alive": False,"can_fly":False,
+                "has_legs": False
+                },
     "zoo": {"is_animal": False, "is_nature": False, "is_place": True,
-             "is_food": False, "is_alive": False,"can_fly":False,"has_legs": False},
-    "Italy": {"is_animal": False, "is_nature": False, "is_place": True,
-               "is_food": False, "is_alive": False,"can_fly":False,"has_legs": False},
+             "is_food": False, "is_alive": False,"can_fly":False,
+             "has_legs": False
+             },
+    "italy": {"is_animal": False, "is_nature": False, "is_place": True,
+               "is_food": False, "is_alive": False,"can_fly":False,
+               "has_legs": False
+               },
     "park": {"is_animal": False, "is_nature": True, "is_place": True,
-              "is_food": False, "is_alive": False,"can_fly":False,"has_legs": False},
+              "is_food": False, "is_alive": False,"can_fly":False,
+              "has_legs": False
+              },
 #                           food
     "burger": {"is_animal": False, "is_nature": False, "is_place": False,
-                "is_food": True, "is_alive": False,"can_fly":False,"has_legs": False},
+                "is_food": True, "is_alive": False,"can_fly":False,
+                "has_legs": False
+                },
     "cracker": {"is_animal": False, "is_nature": False, "is_place": False,
-                 "is_food": True, "is_alive": False,"can_fly":False,"has_legs": False},
+                 "is_food": True, "is_alive": False,"can_fly":False,
+                 "has_legs": False
+                 },
     "banana": {"is_animal": False, "is_nature": True, "is_place": False,
-                "is_food": True, "is_alive": True,"can_fly":False,"has_legs": False},
+                "is_food": True, "is_alive": True,"can_fly":False,
+                "has_legs": False
+                },
     "pizza": {"is_animal": False, "is_nature": False, "is_place": False,
-               "is_food": True, "is_alive": False,"can_fly":False,"has_legs": False},
+               "is_food": True, "is_alive": False,"can_fly":False,
+               "has_legs": False
+               },
     "carrot": {"is_animal": False, "is_nature": True, "is_place": False,
-                "is_food": True, "is_alive": True,"can_fly":False,"has_legs": False}
+                "is_food": True, "is_alive": True,"can_fly":False,
+                "has_legs": False
+                }
 
     }
 
@@ -65,6 +105,18 @@ questions_asked = 0
 
 max_questions = 20
 
+#               FUNCTIONS!!
+
+def answer_question(fact_key):
+#               checks if the answers applys and creating responces for the player
+    if facts[secret_answer][fact_key]:
+        print("yes")
+    else:
+        print("no")
+    
+    global questions_asked
+    questions_asked += 1
+    print("remaining questions:", max_questions - questions_asked)
 #               MAIN GAME LOOP
 
 while questions_asked < max_questions:
@@ -79,23 +131,38 @@ while questions_asked < max_questions:
             questions_asked += 1
             print("remaining questions:", max_questions - questions_asked)
             continue
+    
     elif "animal" in question:
-        if facts[secret_answer]["is_animal"]:
-            print("yes")
+        answer_question("is_animal")
+
     elif "nature" in question:
-        if facts[secret_answer]["is_nature"]:
-            print("yes")
+        answer_question("is_nature")
+
     elif "place" in question:
-        if facts[secret_answer]["is_place"]:
-            print("yes")
+        answer_question("is_place")
+   
     elif "food" in question:
-        pass
+        answer_question("is_food")
+    
     elif "alive" in question:
-        pass
+        answer_question("is_alive")
+    
     elif "fly" in question:
-        pass
-    elif "legs" in question:
-        pass
+        answer_question("can_fly")
+    
+    elif "leg" in question:
+        answer_question("has_legs")
+    
+    else:
+        print("sorry thats not in the database")
+        questions_asked += 1
+        print("remaining questsions:",max_questions - questions_asked)
+
+if questions_asked >= max_questions:
+    print("\n🛑 Game over! You used all your questions.")
+    print("The correct answer was:", secret_answer)
+    
+
 
 
 
