4a4b55cea7480a085444ebf404843efa4e1b9fc5
yahyathedev13 <yahy.ahmed.dev.123@gmail.com>
2025-08-06 14:06:01 -0700

    Checking if my window's computer all set for pushing

---
+++ 2d_game_graphics_1/spark-hopper copy/scripts/area_enter_checker.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_body_exited(body: Node2D) -> void:
+	$AnimationPlayer.play("words")
+	monitoring = false
---
+++ python_1/20_questions.py
@@ -0,0 +1,221 @@
+import random
+
+print("\nlets play a version of 20 question: ask valid questions with keywords [legs,animals,alive,nature,place,food,fly] \n")
+
+#                  MAIN VARIABLES
+
+possible_guesses = [
+   
+    "tree","grass","river","mountain","flower",   
+    "elephant","tiger","giraffe","monkey","hawk",
+    "paris","school","zoo","italy","park",
+    "burger","cracker","banana","pizza","carrot"
+   
+    ]
+
+facts = {
+
+#                       animals
+   
+    "elephant":{"is_animal": True, "is_nature": True, "is_place": False,
+                 "is_food":False, "is_alive":True,"can_fly":False,
+                 "has_legs": True
+                 },
+   
+    "tiger":{"is_animal": True, "is_nature": True, "is_place": False,
+              "is_food":False, "is_alive":True,"can_fly":False,
+              "has_legs": True
+              },
+   
+    "giraffe":{"is_animal": True, "is_nature": True,"is_place": False,
+                "is_food":False, "is_alive":True,"can_fly":False,
+                "has_legs": True
+                },
+   
+    "monkey":{"is_animal": True, "is_nature": True,"is_place": False,
+               "is_food":False, "is_alive":True,"can_fly":False,
+               "has_legs": True
+               },
+   
+    "hawk":{"is_animal": True, "is_nature": True, "is_place": False,
+             "is_food":False, "is_alive":True,"can_fly":True,
+             "has_legs": False
+             },
+#                         nature
+   
+    "tree": {"is_animal": False, "is_nature": True, "is_place": False,
+              "is_food": False, "is_alive": True,"can_fly":False,
+              "has_legs": False
+              },
+   
+    "grass": {"is_animal": False, "is_nature": True, "is_place": False,
+               "is_food": False, "is_alive": True,"can_fly":False,
+               "has_legs": False
+               },
+    
+    "river": {"is_animal": False, "is_nature": True, "is_place": False,
+               "is_food": False, "is_alive": False,"can_fly":False,
+               "has_legs": False
+               },
+   
+    "mountain": {"is_animal": False, "is_nature": True, "is_place": False,
+                  "is_food": False, "is_alive": False,"can_fly":False,
+                  "has_legs": False
+                  },
+   
+    "flower": {"is_animal": False, "is_nature": True, "is_place": False,
+                "is_food": False, "is_alive": True,"can_fly":False,
+                "has_legs": False
+                },
+#                          places
+  
+    "paris": {"is_animal": False, "is_nature": False, "is_place": True,
+               "is_food": False, "is_alive": False,"can_fly":False,
+               "has_legs": False
+               },
+    
+    "school": {"is_animal": False, "is_nature": False, "is_place": True,
+                "is_food": False, "is_alive": False,"can_fly":False,
+                "has_legs": False
+                },
+   
+    "zoo": {"is_animal": False, "is_nature": False, "is_place": True,
+             "is_food": False, "is_alive": False,"can_fly":False,
+             "has_legs": False
+             },
+   
+    "italy": {"is_animal": False, "is_nature": False, "is_place": True,
+               "is_food": False, "is_alive": False,"can_fly":False,
+               "has_legs": False
+               },
+   
+    "park": {"is_animal": False, "is_nature": True, "is_place": True,
+              "is_food": False, "is_alive": False,"can_fly":False,
+              "has_legs": False
+              },
+#                           food
+   
+    "burger": {"is_animal": False, "is_nature": False, "is_place": False,
+                "is_food": True, "is_alive": False,"can_fly":False,
+                "has_legs": False
+                },
+    
+    "cracker": {"is_animal": False, "is_nature": False, "is_place": False,
+                 "is_food": True, "is_alive": False,"can_fly":False,
+                 "has_legs": False
+                 },
+    
+    "banana": {"is_animal": False, "is_nature": True, "is_place": False,
+                "is_food": True, "is_alive": True,"can_fly":False,
+                "has_legs": False
+                },
+    
+    "pizza": {"is_animal": False, "is_nature": False, "is_place": False,
+               "is_food": True, "is_alive": False,"can_fly":False,
+               "has_legs": False
+               },
+   
+    "carrot": {"is_animal": False, "is_nature": True, "is_place": False,
+                "is_food": True, "is_alive": True,"can_fly":False,
+                "has_legs": False
+                }
+
+    }
+
+secret_answer = random.choice(possible_guesses)
+
+questions_asked = 0
+
+max_questions = 20
+
+#               FUNCTIONS!!
+
+def answer_question(fact_key):
+#               checks if the answers applys and creating responces for the player
+    if facts[secret_answer][fact_key]:
+        print("yes")
+    else:
+        print("no")
+    
+    global questions_asked
+    questions_asked += 1
+    print("remaining questions:", max_questions - questions_asked)
+
+#               MAIN GAME LOOP
+
+while questions_asked < max_questions:
+
+#               Asks the player the question    
+    
+    question = input("\nAsk a yes or no question or type 'guess' when you have an answer(you can also type 'quit' to give up):\n").lower()
+    
+    if question == "guess":
+        guess = input("\nwhat is your guess\n")
+        
+        if guess == secret_answer:
+            print("\ncongradulations you have guessed the answer;",secret_answer+",great job\n")
+            print("you only guessed in",questions_asked)
+            break
+        
+        else:
+            print("\nsorry thats not it\n")
+            questions_asked += 1
+            print("remaining questions:", max_questions - questions_asked)
+            
+            continue
+    
+    if question == "quit":
+        quit_assurance = input("are you sure you want to give up('yes'/'no')")
+        if quit_assurance == "yes":
+            print("\n Game over! You quit your challenge :( ")
+            print("The correct answer was:", secret_answer)
+           
+            break
+        
+        elif quit_assurance == "no":
+            print("\nok lets continue the game -> \n")
+            
+            continue
+        else:
+            print("i dont quite understand that. lets continue -> \n")
+           
+            continue
+
+
+    
+    elif "animal" in question:
+        answer_question("is_animal")
+
+    elif "nature" in question:
+        answer_question("is_nature")
+
+    elif "place" in question:
+        answer_question("is_place")
+   
+    elif "food" in question:
+        answer_question("is_food")
+    
+    elif "alive" in question:
+        answer_question("is_alive")
+    
+    elif "fly" in question:
+        answer_question("can_fly")
+    
+    elif "leg" in question:
+        answer_question("has_legs")
+    
+    else:
+        print("sorry thats not in the database")
+        questions_asked += 1
+        print("remaining questsions:",max_questions - questions_asked)
+
+if questions_asked >= max_questions:
+    print("\n🛑 Game over! You used all your questions.")
+    print("The correct answer was:", secret_answer)
+    
+
+
+
+
+
+
